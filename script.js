639ea3b740850e9f185ef89c210e45ca5c19bf58
mchung03 <minhee.chung14@gmail.com>
2023-06-29 12:18:49 -0700

    wrote else statements with possible combinations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var rPS= ["Rock", "Paper", "Scissors"] 
+var rPS= ["R", "P", "S"] 
 var results = {
     Wins: 0,
     Losses: 0,
@@ -8,8 +8,21 @@ var results = {
 var playerChoice = prompt("Please choose R,P,S")
 
 function playGame(playerChoice) {
-    var cpuChoice = Math.floor(Math.random() * rPS.length)
+    var cpuChoice = rPS[Math.floor(Math.random() * rPS.length)]
 
-    if (playerChoice){}
+    if (playerChoice === cpuChoice){
+        alert("Tied!")
+    } else if (playerChoice === "R" && cpuChoice === "P") {
+        alert("You lost!")
+    } else if (playerChoice === "R" && cpuChoice === "S") {
+        alert("You won!")
+    } else if (playerChoice === "S" && cpuChoice === "R") {
+        alert("You lost!")
+    } else if (playerChoice === "S" && cpuChoice === "P") {
+        alert("You won!")
+    } else if (playerChoice === "P" && cpuChoice === "S") {
+        alert("You lost!")
+    } else if (playerChoice === "P" && cpuChoice === "R") {
+        alert("You won!")
+    }
 }
-
