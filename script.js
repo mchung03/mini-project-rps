1a4c62a5cc6d61efee73e6fa6540194ec470b1ae
Jay Liu <jayliu89@gmail.com>
2023-06-29 11:49:14 -0700

    initial variables declared. function playGame created

---
+++ script.js
@@ -0,0 +1,15 @@
+var rPS= ["Rock", "Paper", "Scissors"] 
+var results = {
+    Wins: 0,
+    Losses: 0,
+    Ties: 0
+}
+
+var playerChoice = prompt("Please choose R,P,S")
+
+function playGame(playerChoice) {
+    var cpuChoice = Math.floor(Math.random() * rPS.length)
+
+    if (playerChoice){}
+}
+
